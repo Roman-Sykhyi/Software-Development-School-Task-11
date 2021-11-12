cecb03dabbd4157fa3d997355cdb5f1e4d24d2cc
Roman-Sykhyi <kariys2015@gmail.com>
2021-11-12 14:44:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Завдання_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
